44d615bf8d71b52fe659a9b0f81d4088b70414e0
Paweł <pawel.bednarz2@gmail.com>
2021-01-15 00:15:17 +0100

    Implement parameter validation

--- app/controllers/RecipientController.class.php
+++ app/controllers/RecipientController.class.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use core\App;
 use core\Message;
+use core\ParamUtils;
 use core\SessionUtils;
 use core\Validator;
 
@@ -12,11 +13,20 @@ class RecipientController {
     public function action_showRecipients() {
         App::getSmarty()->assign("description", "Odbiorcy emaili");
         App::getSmarty()->assign("recipients", $this->getRecipients());
-        App::getSmarty()->display("recipientsDashboard.tpl");
+        $this->renderTemplate("recipientsDashboard.tpl");
     }
 
     public function action_addRecipient() {
+        $firstName = ParamUtils::getFromPost("first_name");
+        $lastName = ParamUtils::getFromPost("last_name");
+        $email = ParamUtils::getFromPost("email");
 
+        if ($this->validateRecipientData($firstName, $lastName, $email)) {
+            $this->addRecipient($firstName, $lastName, $email);
+            App::getMessages()->addMessage(new Message("Pomyślnie dodano odbiorcę", Message::INFO));
+        }
+
+        $this->renderTemplate("addRecipient.tpl");
     }
 
     public function action_editRecipient() {
@@ -43,7 +53,7 @@ class RecipientController {
             }
         }
         App::getSmarty()->assign("recipients", $this->getRecipients());
-        App::getSmarty()->display("recipientsDashboard.tpl");
+        $this->renderTemplate("recipientsDashboard.tpl");
     }
 
     private function getRecipients() {
@@ -52,11 +62,61 @@ class RecipientController {
         ]);
     }
 
+    private function addRecipient($firstName, $lastName, $email) {
+        return App::getDB()->insert("recipient", [
+            "uuid"=>generate_uuid(),
+            "first_name"=>$firstName,
+            "last_name"=>$lastName,
+            "email"=>$email,
+            "user_uuid"=>SessionUtils::load("userUuid", load)
+        ]);
+    }
+
     private function deleteRecipient($recipientUuid) {
         return App::getDB()->delete("recipient", [
             "uuid"=>$recipientUuid,
             "user_uuid"=>SessionUtils::load("userUuid", true)
         ]);
+    }
+
+    private function validateRecipientData($firstName, $lastName, $email) {
+        $v = new Validator();
+        $firstName = $v->validate($firstName, [
+            "required"=>true,
+            "required_message"=>'Parametr "Imię" jest wymagany',
+            "escape"=>true,
+            "trim"=>true,
+            "min_length"=>2,
+            "max_length"=>60,
+            "validator_message"=>'"Imię" powinno zawierać od 2 do 60 znaków'
+        ]);
+
+        $lastName = $v->validate($lastName, [
+            "required"=>true,
+            "required_message"=>'Parametr "Nazwisko" jest wymagany',
+            "escape"=>true,
+            "trim"=>true,
+            "min_length"=>2,
+            "max_length"=>80,
+            "validator_message"=>'"Nazwisko" powinno zawierać od 2 do 80 znaków'
+        ]);
+
+        $email = $v->validate($email, [
+            "required"=>true,
+            "required_message"=>'Parametr "Email" jest wymagany',
+            "escape"=>true,
+            "email"=>true,
+            "trim"=>true,
+            "min_length"=>5,
+            "max_length"=>60,
+            "validator_message"=>'"Email" powinno zawierać od 2 do 60 znaków'
+        ]);
+
+        return !App::getMessages()->isError();
+    }
+
+    private function renderTemplate($template) {
+        App::getSmarty()->display($template);
     }
 
 }
