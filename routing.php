6e890f580ad87dc5dbac12cecf27ae542bd8c6ba
Paweł <pawel.bednarz2@gmail.com>
2021-01-18 16:33:31 +0100

    Refactor routing.php to stop using UserController

--- routing.php
+++ routing.php
@@ -6,8 +6,6 @@ use core\Utils;
 App::getRouter()->setDefaultRoute('dashboard'); #default action
 App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
-Utils::addRoute('addUser', 'UserController');
-
 // login / logout routes
 Utils::addRoute('login', 'LoginController');
 Utils::addRoute('logout', 'LoginController');
@@ -19,7 +17,7 @@ Utils::addRoute('addEntry', 'EntryController', "user");
 //Utils::addRoute('editEntry', 'EntryController', "user");
 Utils::addRoute('deleteEntry', 'EntryController', "user");
 
-//// recepient routes
+// recepient routes
 Utils::addRoute('showRecipients', 'RecipientController', "user");
 Utils::addRoute('addRecipient', 'RecipientController', "user");
 //Utils::addRoute('editRecipient', 'RecipientController', "user");
@@ -31,23 +29,23 @@ Utils::addRoute('addEmailTemplate', 'EmailTemplateController', "user");
 //Utils::addRoute('editEmailTemplate', 'EmailTemplateController', "user");
 Utils::addRoute('deleteEmailTemplate', 'EmailTemplateController', "user");
 
-//// report routes
+// report routes
 //Utils::addRoute('showReports', 'ReportController', "user");
 //Utils::addRoute('generateReport', 'ReportController', "user");
 //Utils::addRoute('deleteReport', 'ReportController', "user");
-//
-//// send email routes
+
+// send email routes
 //Utils::addRoute('sendEmail', 'EmailSendController', "user");
 //Utils::addRoute('sendReport', 'EmailSendController', "user");
-//
+
 //Utils::addRoute('showEmails', 'EmailSendController', "user");
 //Utils::addRoute('showEmail', 'EmailSendController', "user");
 //Utils::addRoute('deleteEmail', 'EmailSendController', "user");
-//
+
 // administrative routes
 Utils::addRoute('adminDashboard', 'AdminController', "admin");
-//
+
 // user routes (only for administrator)
-//Utils::addRoute('addUser', 'UserController', "admin");
-//Utils::addRoute('editUser', 'UserController', "admin");
-//Utils::addRoute('deleteUser', 'UserController', "admin");
+Utils::addRoute('addUser', 'AdminController', "admin");
+//Utils::addRoute('editUser', 'AdminController', "admin");
+Utils::addRoute('deleteUser', 'AdminController', "admin");
