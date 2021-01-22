9fed396eb3deba86885c0989464d2200a29ea13b
Paweł <pawel.bednarz2@gmail.com>
2021-01-22 15:59:20 +0100

    Implement Content-Security-Policy header

--- public/index.php
+++ public/index.php
@@ -2,4 +2,7 @@
 require_once '../init.php';
 use core\App;
 
+// TODO download needed fonts to use locally
+header("Content-Security-Policy: default-src 'self' fonts.googleapis.com fonts.gstatic.com");
+
 include App::getConf()->root_path.App::getConf()->action_script;
