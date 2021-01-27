cf54a598ca17bacc209b3c534582bccdeb44fc17
Paweł <pawel.bednarz2@gmail.com>
2021-01-27 20:19:17 +0100

    Implement send email form display

--- app/controllers/EmailSendController.class.php
+++ app/controllers/EmailSendController.class.php
@@ -16,11 +16,29 @@ class EmailSendController {
     }
 
     public function action_sendEmail() {
-
+        $this->renderSendEmail();
     }
 
     private function getEmails() {
         return App::getDB()->select("sent_email", "*", [
+            "user_uuid"=>SessionUtils::load("userUuid", true)
+        ]);
+    }
+
+    private function getTemplates() {
+        return App::getDB()->select("email_template", "*", [
+            "user_uuid"=>SessionUtils::load("userUuid", true)
+        ]);
+    }
+
+    private function getRecipients() {
+        return App::getDB()->select("recipient", "*", [
+            "user_uuid"=>SessionUtils::load("userUuid", true)
+        ]);
+    }
+
+    private function getReports() {
+        return App::getDB()->select("report", "*", [
             "user_uuid"=>SessionUtils::load("userUuid", true)
         ]);
     }
@@ -31,4 +49,11 @@ class EmailSendController {
         App::getSmarty()->display("emailsTable.tpl");
     }
 
+    private function renderSendEmail() {
+        App::getSmarty()->assign("description", "Wyślij email");
+        App::getSmarty()->assign("templates", $this->getTemplates());
+        App::getSmarty()->assign("recipients", $this->getRecipients());
+        App::getSmarty()->assign("reports", $this->getReports());
+        App::getSmarty()->display("sendEmail.tpl");
+    }
 }
