e38b6e396e5d53a2d884d3272fd495144814e965
Paweł <pawel.bednarz2@gmail.com>
2021-01-30 22:08:01 +0100

    Add more security headers

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ require_once '../init.php';
 use core\App;
 
 // TODO download needed fonts to use locally
-header("Content-Security-Policy: default-src 'self' fonts.googleapis.com fonts.gstatic.com");
+header("Content-Security-Policy: default-src 'self' fonts.googleapis.com fonts.gstatic.com; object-src 'none'; frame-ancestors 'none'; require-trusted-types-for 'script'");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: origin");
+if (strtolower(App::getConf()->protocol) === "https") {
+    header("Strict-Transport-Security: max-age=63072000; includeSubDomains;");
+}
 
 include App::getConf()->root_path.App::getConf()->action_script;
