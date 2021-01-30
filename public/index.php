c1075e19415395ee5ab213de0e2ed6e8d0704b21
Paweł <pawel.bednarz2@gmail.com>
2021-01-30 22:24:06 +0100

    Refactor CSP header

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ require_once '../init.php';
 use core\App;
 
 // TODO download needed fonts to use locally
-header("Content-Security-Policy: default-src 'self' fonts.googleapis.com fonts.gstatic.com; object-src 'none'; frame-ancestors 'none'; require-trusted-types-for 'script'");
+header("Content-Security-Policy: default-src 'self' fonts.googleapis.com fonts.gstatic.com; object-src 'none'; frame-ancestors 'none'");
 header("X-Content-Type-Options: nosniff");
 header("Referrer-Policy: origin");
 if (strtolower(App::getConf()->protocol) === "https") {
