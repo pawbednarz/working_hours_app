0cb42a789b8a1d34cd52381eee76c0372c3e7573
Paweł <pawel.bednarz2@gmail.com>
2021-01-30 16:47:48 +0100

    Add secure cookie configuration

--- init.php
+++ init.php
@@ -35,6 +35,25 @@ else $conf->action_root = $conf->app_root.'/index.php?'.$conf->action_param.'=';
 $conf->action_url = $conf->server_url.$conf->action_root;
 $conf->assets_url = $conf->app_url."/assets/";
 
+// config cookies flags
+$conf->cookie_http_only = true;
+$conf->cookie_secure = false;
+$conf->cookie_same_site = "strict";
+$conf->cookie_path = $conf->app_root;
+$conf->cookie_domain = $conf->server_name;
+if (strtolower($conf->protocol) === "https") {
+    $conf->cookie_secure = true;
+}
+
+session_set_cookie_params([
+    "httponly"=>$conf->cookie_http_only,
+    "secure"=>$conf->cookie_secure,
+    "samesite"=>$conf->cookie_same_site,
+    "path"=>$conf->cookie_path,
+    "domain"=>$conf->cookie_domain
+]);
+session_name("sessionId");
+
 App:: createAndInitialize($conf);
 
 // store globally used variable go have access to them from every script
